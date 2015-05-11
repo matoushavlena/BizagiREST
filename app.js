903fdc18e1282a9ea8b5e95c327f98037217ff56
matoushavlena <matoushavlena@matouss-mbp.home>
2015-05-10 23:30:10 -0400

    changes

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 var app			= require('express')(),
 	express 	= require('express'),
 	http		= require('http'),
-	bodyParser	= require('body-parser');
+	bodyParser	= require('body-parser'),
+	server = require('http').Server(app);
 
-app.set('port', process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
+app.set('port', port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var slot = {};
+var labs = {};
+labs["Yorktown"] = true;
+slot["2015-01-01"] = true;
 
-app.get('/session', function(req, res) {
-
+app.get('/isAvailable/:lab/:timestamp', function(req, res) {
+	res.send(200);
+	/*if (!labs.hasOwnProperty(req.params.lab)) res.send(503);
+	else {
+		var timestamp = Date.parse(req.params.timestamp)
+		if (isNaN(timestamp)==false) {
+		    var d = new Date(timestamp);
+		    res.send("ok");
+		} else {
+			res.send(503);
+		}
+	}*/
 });
 
 app.post('/session/login', function(req, res) {
 
 });
 
-
-server.listen(process.env.PORT, function(){  
-	console.log('Express server listening on port '+ process.env.PORT);
+server.listen(port, function(){  
+	console.log('Express server listening on port '+ port);
 });
